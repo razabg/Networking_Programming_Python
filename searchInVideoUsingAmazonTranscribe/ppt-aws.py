b72a14b93e11ebce9a4f52f7d67d296c02fb73a1
Raz abergel <80701759+razabg@users.noreply.github.com>
2022-12-25 10:37:15 +0200

    Update ppt-aws.py

--- searchInVideoUsingAmazonTranscribe/ppt-aws.py
+++ searchInVideoUsingAmazonTranscribe/ppt-aws.py
@@ -8,8 +8,8 @@ import os
 
 import boto3
 
-AWS_ACCESS_KEY_ID = 'T7HMYR7NX7T' # insert here your real access key code
-AWS_SECRET_ACCESS_KEY = 'gavJNEoOtvpzGDK9Svp+h'
+AWS_ACCESS_KEY_ID = 'T7HMYxxxxxxxx' # insert here your real access key code
+AWS_SECRET_ACCESS_KEY = 'gavJNExxxxxxxx'
 REGION = 'eu-central-1'
 
 transcribe = boto3.client('transcribe',
