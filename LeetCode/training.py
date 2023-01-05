4fe94cd01d0d191767de23a1991fbaca5adf0a77
razabg <razabg@gmail.com>
2023-01-05 17:37:42 +0200

    syn done.

--- LeetCode/training.py
+++ LeetCode/training.py
@@ -1,8 +1,35 @@
+from collections import deque
+
+
+# Given a string s containing just the characters '(', ')', '{', '}', '[' and ']', determine if the input string is
+# valid.
+#
+# An input string is valid if:
+#
+# Open brackets must be closed by the same type of brackets.
+# Open brackets must be closed in the correct order.
+# Every close bracket has a corresponding open bracket of the same type. "{ [ ] }"
+class Solution:
+    def isValid(self, s: str) -> bool:
+        Map = {")": "(", "]": "[", "}": "{"}
+        stack = []
+
+        for c in s:
+            if c not in Map:
+                stack.append(c)
+                continue
+            if not stack or stack[-1] != Map[c]:
+                return False
+            stack.pop()
+
+        return not stack
 
 
 
 def main():
-
+    sol = Solution()
+    b = sol.isValid("{[]}")
+    print(b)
 
 if __name__ == '__main__':
     main()
---
+++ ex7.synFloodTcpResearch/synFloodTcp.py
@@ -0,0 +1,75 @@
+# ex7.Syn Flood attack research.
+# examine pcap file and look for the suspicious addresses that might attack the server and write them in a file.
+# Author: Raz abergel.
+
+"""THE ALGORITHM:
+***  I used 4 factors in order to detect suspicious addresses  ***
+all of these factors based on the idea that the attacker
+want to sent lots of syn packets in a short time without responding to the
+syn-ack packets that the server will send him back.
+I want to track every activity that will be suspicious with the following factors:
+1.Count the syn requests of every ip address
+2.Count the ack requests of every ip address,if the ip has syn packets and has no ack packets at all,it is suspicious.
+3.Determine a specific threshold that every ip that passed the limit will be suspicious.
+4.calc the rate of sending ack packets per seconds --
+ -- (using the time of the first packet and the last packet),if the rate is too high it is suspicious.
+(I determent that above 3 packets per second is too high)"""
+
+from collections import defaultdict
+from scapy.layers.inet import TCP, IP
+from scapy.utils import rdpcap
+
+# Set the threshold for the number of SYN packets received from a single IP
+THRESHOLD = 9
+
+# Set the threshold for the rate at which SYN packets are received (in packets per second)
+RATE_THRESHOLD = 3
+
+
+def main():
+    # Create dictionaries to track the number of SYN and ACK packets received from each IP
+    syn_counter = defaultdict(int)
+    ack_counter = defaultdict(int)
+
+    # Read the pcap file and iterate over the packets
+    packets = rdpcap(r'C:\NETWORKS\works\ex7.synFloodTcpResearch\SYNflood.pcapng')
+
+    start_time = packets[0].time  # store the time of the first packet as the start time
+    end_time = packets[-1].time  # store the time of the last packet as the end time
+
+    for packet in packets:
+        # Check if the packet is a SYN packet
+        if TCP in packet and packet[TCP].flags == 'S':
+            # Increment the SYN counter for this IP address
+            syn_counter[packet[IP].src] += 1
+        # Check if the packet is an ACK packet
+        elif TCP in packet and packet[TCP].flags == 'A':
+            # Increment the ACK counter for this IP address
+            ack_counter[packet[IP].src] += 1
+
+    # Calculate the elapsed time of the attack
+    elapsed_time = end_time - start_time
+
+    # Open a file for writing
+    with open('suspiciousIps.txt', 'w') as f:
+        # Iterate over the dictionaries and check for mismatches between SYN and ACK counts
+        for ip, syn_counter in syn_counter.items():
+            # Calculate the rate at which SYN packets were received from this IP
+            rate = syn_counter / elapsed_time
+            # if the syn counter is above the limit and on the top of that
+            # this address didn't return any ack to the server.
+            if syn_counter > THRESHOLD and ack_counter[ip] == 0:
+                f.write(f'{ip}\n')
+                continue
+            # still suspicious about address that has more syn then ack
+            if ack_counter[ip] != 0 and syn_counter > ack_counter[ip]:
+                f.write(f'{ip}\n')
+                continue
+            # Check if the rate exceeds the threshold
+            if rate > RATE_THRESHOLD:
+                f.write(f'{ip}\n')
+
+
+if __name__ == "__main__":
+    # Call the main handler function
+    main()
